f905d853df8b276c627e087a09b09fede96c0e7b
alliemarshall99 <alliemarshall99@gmail.com>
2023-06-14 19:42:31 -0600

    added superuser and admin model

--- MP_Table/models.py
+++ MP_Table/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class People(models.Model):
+    date_missing = models.DateField()
+    last_name = models.CharField(max_length=30)
+    first_name = models.CharField(max_length=30)
+    age_at_missing = models.IntegerField()
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=2)
+    gender = models.CharField(max_length=1)
+    race = models.CharField(max_length=1)
+    
+    def __str__(self):
+        return '%s %s' % (self.first_name, self.last_name)
+    
