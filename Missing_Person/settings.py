8bb3a9686bd30005b7f30b53c20daa769419e020
trevshum20 <90296736+trevshum20@users.noreply.github.com>
2023-06-14 18:31:09 -0600

    fixed static

--- Missing_Person/settings.py
+++ Missing_Person/settings.py
@@ -50,6 +50,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+# SECURE_CONTENT_TYPE_NOSNIFF = False
+
+
 ROOT_URLCONF = 'Missing_Person.urls'
 
 TEMPLATES = [
@@ -117,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
         os.path.join(BASE_DIR, 'Missing_Person/static')
 ] 
